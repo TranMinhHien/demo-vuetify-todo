c107093a934815009d229773383be467f0eb2072
TranMinhHien <hientm.info2020@gmail.com>
2022-06-02 17:30:37 +0700

    - update WireFrames

--- vuetify-todo/src/views/AboutView.vue
+++ vuetify-todo/src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis est,
+      nemo officia vitae non corrupti atque repudiandae voluptatibus vel facere.
+      Numquam accusantium error nesciunt sed mollitia temporibus, quibusdam
+      nulla voluptatem.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis est,
+      nemo officia vitae non corrupti atque repudiandae voluptatibus vel facere.
+      Numquam accusantium error nesciunt sed mollitia temporibus, quibusdam
+      nulla voluptatem.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis est,
+      nemo officia vitae non corrupti atque repudiandae voluptatibus vel facere.
+      Numquam accusantium error nesciunt sed mollitia temporibus, quibusdam
+      nulla voluptatem.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis est,
+      nemo officia vitae non corrupti atque repudiandae voluptatibus vel facere.
+      Numquam accusantium error nesciunt sed mollitia temporibus, quibusdam
+      nulla voluptatem.
+    </p>
   </div>
 </template>
